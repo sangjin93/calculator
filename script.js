cd4e1a17a815411cfee5cf32ebf67844ddf317c8
Sean Yoon <seanyoonsy@gmail.com>
2022-07-21 05:21:09 -0700

    Create 'operate' function that takes an operator and 2 numbers, then calls on one of add, subtract, multiply, divide functions

--- script.js
+++ script.js
@@ -13,3 +13,23 @@ function multiply(a, b) {
 function divide(a, b) {
     return a / b;
 };
+
+function operate(operator, a, b) {
+    a = Number(a);
+    b = Number(b);
+
+    switch (operator) {
+      case '+':
+        return add(a, b);
+      case '-':
+        return subtract(a, b);
+      case 'x':
+        return multiply(a, b);
+      case '÷':
+        if (b === 0) {
+            return null
+        } else return divide(a, b);
+      default:
+        return null;
+    }
+};
