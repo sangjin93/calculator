b383e56be63489cbb9843f461e37e58a7f719a9c
Sean Yoon <seanyoonsy@gmail.com>
2022-07-23 21:02:18 -0700

    Create list of const variables in script.js for buttons and the screen's operations

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+const numberButtons = document.querySelectorAll('[data-number]')
+const operatorButtons = document.querySelectorAll('[data-operator]')
+const equalsButton = document.getElementById('equalsBtn')
+const clearButton = document.getElementById('clearBtn')
+const deleteButton = document.getElementById('deleteBtn')
+const pointButton = document.getElementById('pointBtn')
+const lastOperationScreen = document.getElementById('lastOperationScreen')
+const currentOperationScreen = document.getElementById('currentOperationScreen'
+
 function add(a, b) {
     return a + b;
 };
